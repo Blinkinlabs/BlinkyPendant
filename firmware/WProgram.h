8852d5c17748e560c2e26a66246e2c8b156c935e
Matt Mets <matt@blinkinlabs.com>
2017-05-10 15:23:51 +0800

    Remove conflicting math function

--- firmware/WProgram.h
+++ firmware/WProgram.h
@@ -21,17 +21,6 @@ uint16_t makeWord(byte h, byte l);
 
 unsigned long pulseIn(uint8_t pin, uint8_t state, unsigned long timeout = 1000000L);
 
-void tone(uint8_t pin, uint16_t frequency, uint32_t duration = 0);
-void noTone(uint8_t pin);
-
-// WMath prototypes
-uint32_t random(void);
-uint32_t random(uint32_t howbig);
-int32_t random(int32_t howsmall, int32_t howbig);
-void randomSeed(uint32_t newseed);
-void srandom(uint32_t newseed);
-long map(long, long, long, long, long);
-
 #include "pins_arduino.h"
 
 #endif // __cplusplus
